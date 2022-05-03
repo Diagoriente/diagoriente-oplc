90d9caea3f978022a32986d327f80372cdfd4d36
Guillaume Chérel <guillaume.cherel@cosimus.com>
2022-05-03 19:55:07 +0200

    Simplify script.

--- release-production.sh
+++ release-production.sh
@@ -7,32 +7,29 @@ VERSION=$(./version.sh)
 TMPDIR="/tmp/diagoriente-oplc/"
 echo "Created temp dir $TMPDIR"
 
-DOCKER_IMAGE_LOCAL_FRONTEND=$TMPDIR/docker_images/$VERSION/frontend.tar
-DOCKER_IMAGE_LOCAL_BACKEND=$TMPDIR/docker_images/$VERSION/backend.tar
+DOCKER_IMAGE_LOCAL=$TMPDIR/docker_images/$VERSION/all.tar
 
 mkdir -p $TMPDIR/docker_images/$VERSION
-echo "Saving docker image $COMPOSE_PROJECT_NAME/frontend:$VERSION"
-docker save $COMPOSE_PROJECT_NAME/frontend:$VERSION \
-    -o $DOCKER_IMAGE_LOCAL_FRONTEND
-echo "Saving docker image $COMPOSE_PROJECT_NAME/backend:$VERSION"
-docker save $COMPOSE_PROJECT_NAME/backend:$VERSION \
-    -o $DOCKER_IMAGE_LOCAL_BACKEND
+echo "Saving docker images to $DOCKER_IMAGE_LOCAL"
+docker save \
+    $COMPOSE_PROJECT_NAME/frontend:$VERSION \
+    $COMPOSE_PROJECT_NAME/backend:$VERSION \
+    $COMPOSE_PROJECT_NAME/streamlit:$VERSION \
+    -o $DOCKER_IMAGE_LOCAL
 
 RELEASE="${VERSION}-$(date -u --iso=seconds)"
 
 echo "Sending docker images."
 ssh ovh-vps-test "mkdir -p Diagoriente-Oplc/docker_images/$VERSION"
 rsync -avHXS --numeric-ids --info=progress2 \
-    $TMPDIR/docker_images/$VERSION/frontend.tar \
-    $TMPDIR/docker_images/$VERSION/backend.tar \
+    $TMPDIR/docker_images/$VERSION/all.tar \
     ovh-vps-test:Diagoriente-Oplc/docker_images/$VERSION/
 
 echo "Loading docker images on the server."
-ssh ovh-vps-test "docker load -i Diagoriente-Oplc/docker_images/$VERSION/backend.tar"
-ssh ovh-vps-test "docker load -i Diagoriente-Oplc/docker_images/$VERSION/frontend.tar"
+ssh ovh-vps-test "docker load -i Diagoriente-Oplc/docker_images/$VERSION/all.tar"
 
 echo "Creating release ovh-vps-test:Diagoriente-Oplc/$RELEASE"
-ssh ovh-vps-test mkdir -p Diagoriente-Oplc/releases/$RELEASE
+ssh ovh-vps-test "mkdir -p Diagoriente-Oplc/releases/$RELEASE"
 rsync -av --info=progress2 .env-production ovh-vps-test:Diagoriente-Oplc/releases/$RELEASE/.env
 rsync -av --info=progress2 \
     .version \
