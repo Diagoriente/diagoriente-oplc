d9c58851400a23729a8970919b4110b4fd5529ca
Guillaume Chérel <guillaume.cherel@cosimus.com>
2022-05-04 08:56:03 +0200

    Remove duplicate case-study.

--- case-studies/main.py
+++ case-studies/main.py
@@ -3,7 +3,6 @@ import skill_importance_measure
 
 pages = {
         "Comparaison de mesures de centralité": skill_importance_measure.run,
-        "Comparaison de mesures de centralité2": skill_importance_measure.run,
         }
 
 page = st.selectbox("Cas d'étude:", list(pages.keys()))
