7394e5bb1010476aa790d41e4aec6ddc7448ae89
Guillaume Chérel <guillaume.cherel@cosimus.com>
2022-05-24 13:04:34 +0200

    Add skill distance based on coocurrence.

--- case-studies/skill_distance.py
+++ case-studies/skill_distance.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import oplc.model
 from oplc.core import (
-        job_recommendation, ExperienceId, JobId, JobRecommendation, SkillId,
+        job_recommendation, ExperienceId, JobId, JobRecommendation, SkillId, jobs_from_skills,
         skill_graph,
         )
 import matplotlib
@@ -15,6 +15,10 @@ from bokeh.models import ColumnDataSource
 from sklearn.manifold import TSNE
 from typing import Callable
 from scipy.spatial.distance import pdist, squareform
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+rng = np.random.default_rng()
 
 plt.ioff()
 
@@ -30,6 +34,9 @@ skills = oplc.model.model.skills
 assert nx.is_connected(sg)
 
 
+skills_jobs = oplc.model.model.jobs_skills.df.transpose()
+# Remove skills that are not associated to any job
+skills_jobs = skills_jobs.loc[skills_jobs.sum(axis=1) > 0, :]
 
 
 
@@ -63,23 +70,50 @@ def show_skills(dist):
 
 
     dist_long = (dist.unstack().sort_values().reset_index()
-      .loc[lambda x: x.level_0 != x.level_1,:]
+      # Remove distances to self and redundant pairs
+      .loc[lambda x: x.level_0 < x.level_1,:]
       .assign(skill1=lambda x: [skills[i].name for i in x.level_0])
       .assign(skill2=lambda x: [skills[i].name for i in x.level_1])
       .drop(columns=["level_0", "level_1"])
       .rename(columns={0: "distance"})
       )
 
-    with st.expander("Exemples de compétences proches"):
+    with st.expander(f"Compétences les plus proches"):
         st.table(dist_long.head(20))
 
-    with st.expander("Exemples de compétences éloignées"):
+    with st.expander(f"Compétences les plus éloignées"):
         st.table(dist_long.tail(20))
+
+
+@st.cache(hash_funcs={st.delta_generator.DeltaGenerator: lambda _: None})
+def dist_cooc(metric, progress_bar):
+    skill_cooc = skills_jobs.dot(skills_jobs.transpose())
+
+    dist_cooc = pa.DataFrame(np.empty(skill_cooc.shape),
+                             index=skill_cooc.index,
+                             columns=skill_cooc.columns,
+                             )
+
+    for i, s in enumerate(skill_cooc.index):
+        progress_bar.progress(i / len(skill_cooc))
+        logging.info(f"[dist_cooc] computing coocurrence between {s} and all other skills")
+        for s2 in skill_cooc.columns:
+            if s < s2:
+                not_ij = (skill_cooc.columns != s) & (skill_cooc.columns != s2)
+                dist_cooc.loc[s, s2] = pdist(skill_cooc.loc[[s,s2], not_ij],
+                                            metric=metric)
+            if s > s2:
+                dist_cooc.loc[s, s2] = dist_cooc.loc[s2, s]
+            if s == s2:
+                dist_cooc.loc[s, s2] = 0
+
+    return dist_cooc
+
 
 
 
 def run():
-    st.header("Longueur de chemin plus court dans le graphe de compétences")
+    st.header("Approche 1: Longueur du chemin le plus court entre deux compétences dans le graphe de compétences")
 
     dist_shortest = pa.DataFrame({u: d for u,d in nx.all_pairs_shortest_path_length(sg)}).sort_index()
 
@@ -87,11 +121,11 @@ def run():
 
 
 
-    st.header("Distance euclidienne entre les vecteurs métiers")
+    st.header("Approche 2: On compare les compétences par rapport aux métiers auxquelles elles sont rattachées.")
+    st.write("Chaque compétence est représentée par un vecteur booléen de taille égale au nombre de métiers où un élément vaut vrai si la compétence est rattachée au métier correspondant.")
 
-    skills_jobs = oplc.model.model.jobs_skills.df.transpose()
-    # Remove skills that are not associated to any job
-    skills_jobs = skills_jobs.loc[skills_jobs.sum(axis=1) > 0, :]
+    st.subheader("Distance euclidienne entre les vecteurs")
+
     dist_jobs_euclidean = pa.DataFrame(
             squareform(pdist(skills_jobs, metric="euclidean")),
             index=skills_jobs.index,
@@ -101,7 +135,7 @@ def run():
 
 
 
-    st.header("Distance corrélation entre les vecteurs métiers")
+    st.subheader("Distance de corrélation entre les vecteurs")
 
     dist_jobs_correlation = pa.DataFrame(
             squareform(pdist(skills_jobs, metric="correlation")),
@@ -112,14 +146,18 @@ def run():
 
 
 
-# fig, ax = plt.subplots(figsize=(10,10))
-# for i in tsne_shortest.index:
-#     ax.text(tsne_shortest.loc[i, 0], tsne_shortest.loc[i, 1], skills[i].name, 
-#             fontsize=10)
-# ax.set_xlim(tsne_shortest.loc[:, 0].min(), tsne_shortest.loc[:, 0].max())
-# ax.set_ylim(tsne_shortest.loc[:, 1].min(), tsne_shortest.loc[:, 1].max())
-# plt.show()
-# 
-# fig, ax = plt.subplots(figsize=(10,10))
-# ax.scatter(tsne_job_sim[:, 0], tsne_job_sim[:, 1])
-# plt.show()
+    st.header("Approche 3: On compare les compétences par rapport aux compétences avec lequelles elles sont associées")
+    st.write("Chaque compétence est représentée par un vecteur d'entiers de taille égale au nombre de compétences où un élément est égal au nombre de fois que la compétence est associée à la compétence correspondante dans les métiers.")
+    st.write("Avant de calculer la distance (euclidienne, correlation, etc) entre deux compétences i et j, on supprime des deux vecteurs associés les éléments en position i et j, sinon les grandes valeurs de ces éléments ont trop de poids dans les calculs.")
+
+    st.subheader("Distance euclidienne")
+    progress = st.progress(0)
+    dist_cooc_euclidean = dist_cooc("euclidean", progress)
+    progress.progress(1.0)
+    show_skills(dist_cooc_euclidean)
+
+    st.subheader("Distance de corrélation")
+    progress = st.progress(0)
+    dist_cooc_correlation = dist_cooc("correlation", progress)
+    progress.progress(1.0)
+    show_skills(dist_cooc_correlation)
